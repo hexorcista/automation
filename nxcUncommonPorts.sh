24586fd767b7b4f5511a3645c20ca3f8a4e75aae
LaQKracha <68617409+LaQKracha@users.noreply.github.com>
2024-09-21 04:11:44 -0600

    Update nxcUncommonPorts.sh

--- nxcUncommonPorts.sh
+++ nxcUncommonPorts.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 # Check if the user has provided a file as an argument
-# File Input Format Expected:
-# <port> <IP>
-
-
-if [ -z "$2" ]; then
-  echo "Usage: $0 <input_file> <protocol>"
+if [ -z "$4" ]; then
+  echo "Usage: $0 <input_file> <protocol> <user/userList> <password/passwordList>"
   exit 1
 fi
 
 input_file="$1"
 protocol="$2"
+user=$3
+password=$4
 
 if [ ! -f "$input_file" ]; then
   echo "File not found!"
@@ -19,5 +17,5 @@ if [ ! -f "$input_file" ]; then
 fi
 
 while read -r port ip; do
-  nxc $protocol $ip -u '' -p '' --port $port
+  nxc $protocol $ip -u $user -p $password --port $port
 done < "$input_file"
